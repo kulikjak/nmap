237740a4d4ba167b99ed00558c4b6ae17fb6f78e
Jakub Kulík <kulikjak@gmail.com>
2020-07-28 17:22:48 +0200

    Remove now obsolete code branches

--- zenmap/zenmapGUI/higwidgets/gtkutils.py
+++ zenmap/zenmapGUI/higwidgets/gtkutils.py
@@ -133,7 +133,7 @@ higwidgets/gtkutils.py
 """
 
 __all__ = ['gtk_version_major', 'gtk_version_minor', 'gtk_version_release',
-           'gtk_constant_name', 'gobject_register']
+           'gtk_constant_name']
 
 import gi
 
@@ -164,13 +164,3 @@ def gtk_constant_name(group, value):
     groups = {'response': group_response}
 
     return groups.get(group, {}).get(value, 'Error: constant value not found')
-
-
-def gobject_register(klass):
-    """
-    Register a given object by calling gobject.type_register.
-
-    This is no longer being used:
-    https://wiki.gnome.org/Projects/PyGTK/WhatsNew28
-    """
-    pass
